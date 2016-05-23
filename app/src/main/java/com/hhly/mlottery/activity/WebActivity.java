eef420007137a1500579a8fa82ff8ddbe258098a
lzfytt <876376994@qq.com>
2016-05-23 12:25:22 +0800

    修改单页加载的评论条数

--- app/src/main/java/com/hhly/mlottery/activity/WebActivity.java
+++ app/src/main/java/com/hhly/mlottery/activity/WebActivity.java
@@ -44,6 +44,8 @@ import com.tencent.tauth.Tencent;
 import com.tencent.tauth.UiError;
 import com.umeng.analytics.MobclickAgent;
 
+import org.apache.http.util.EncodingUtils;
+
 import java.util.HashMap;
 import java.util.Map;
 
@@ -58,8 +60,8 @@ public class WebActivity extends BaseActivity implements OnClickListener, CyanRe
     private ImageView mPublic_img_back;// 返回
     private TextView mPublic_txt_title;// 标题
     private String url;// 要显示的H5网址
-    private String isComment;// 是否隐藏评论和分享
     private String token;// 登录参数
+    private String reqMethod;// 是否传参数
     private String imageurl;// 图片地址
     private String title;// 标题
     private String subtitle;// 副标题
@@ -305,12 +307,12 @@ public class WebActivity extends BaseActivity implements OnClickListener, CyanRe
             mThird = intent.getStringExtra("thirdId");
             infoTypeName = intent.getStringExtra("infoTypeName");
             token = intent.getStringExtra("token");
-            isComment = intent.getStringExtra("isComment");
+            reqMethod = intent.getStringExtra("reqMethod");
             mPublic_txt_title.setText(infoTypeName);
-            if (TextUtils.isEmpty(isComment)) {//不是新闻资讯时候隐藏分享和评论
+            if (TextUtils.isEmpty(token)) {//token为空，说明是资讯，显示分享和评论
                 public_btn_set.setVisibility(View.VISIBLE);
                 scrollview.setVisibility(View.VISIBLE);
-            } else {
+            } else {//不是新闻资讯的时候隐藏分享和评论
                 public_btn_set.setVisibility(View.GONE);
                 scrollview.setVisibility(View.GONE);
             }
@@ -322,8 +324,13 @@ public class WebActivity extends BaseActivity implements OnClickListener, CyanRe
                 }
 
             });
-//            mWebView.postUrl(url, EncodingUtils.getBytes("", "BASE64"));
-            mWebView.loadUrl(url);
+            //其他页产过来的reqMethod为post时，提交token  否则不提交
+            if (reqMethod!=null&&reqMethod.equals("post")){
+                mWebView.postUrl(url, EncodingUtils.getBytes(token, "BASE64"));
+            }else {
+                mWebView.loadUrl(url);
+            }
+//
             L.d("lzf:" + "imageurl=" + imageurl + "title" + title + "subtitle" + subtitle);
 
 //            /**加載成功显示 分享按钮*/
