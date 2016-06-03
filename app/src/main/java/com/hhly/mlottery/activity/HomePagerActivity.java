9fa47df3906845bfd2689d5b58938eb84c71925e
wangg <wangg@13322.com>
2016-06-03 15:01:02 +0800

    Merge branch 'test' into football
    # Conflicts:
    #	app/src/main/java/com/hhly/mlottery/frame/footframe/StadiumFragment.java

--- app/src/main/java/com/hhly/mlottery/activity/HomePagerActivity.java
+++ app/src/main/java/com/hhly/mlottery/activity/HomePagerActivity.java
@@ -14,11 +14,9 @@ import android.os.Bundle;
 import android.os.Environment;
 import android.os.Handler;
 import android.os.Message;
-import android.support.v4.text.TextUtilsCompat;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AlertDialog;
 import android.text.TextUtils;
-import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
 import android.widget.ImageView;
@@ -48,17 +46,10 @@ import com.hhly.mlottery.util.net.VolleyContentFast;
 import com.umeng.analytics.MobclickAgent;
 import com.umeng.message.PushAgent;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
 
 /**
  * 首页Activity
