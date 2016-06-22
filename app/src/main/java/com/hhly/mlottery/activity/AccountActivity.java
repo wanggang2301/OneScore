b2b28b0bccd8798c86e25e281fb7a3ea453b2df7
Yluyao <Yluyao>
2016-06-22 22:32:11 +0800

    个人中心增加修改密码证码成功提示

--- app/src/main/java/com/hhly/mlottery/activity/AccountActivity.java
+++ app/src/main/java/com/hhly/mlottery/activity/AccountActivity.java
@@ -1,10 +1,10 @@
 package com.hhly.mlottery.activity;
 
-import android.app.AlertDialog;
 import android.app.ProgressDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
