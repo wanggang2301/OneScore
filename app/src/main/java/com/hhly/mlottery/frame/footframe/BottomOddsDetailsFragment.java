8b528382d696daae19e746313cf4a7ba61523693
wangg <wangg@13322.com>
2016-06-30 16:01:29 +0800

    修改不同系统状态栏

--- app/src/main/java/com/hhly/mlottery/frame/footframe/BottomOddsDetailsFragment.java
+++ app/src/main/java/com/hhly/mlottery/frame/footframe/BottomOddsDetailsFragment.java
@@ -2,6 +2,7 @@ package com.hhly.mlottery.frame.footframe;
 
 import android.app.Dialog;
 import android.content.Context;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
@@ -13,6 +14,7 @@ import android.support.design.widget.CoordinatorLayout;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
@@ -123,6 +125,9 @@ public class BottomOddsDetailsFragment extends BottomSheetDialogFragment {
     @Override
     public void setupDialog(final Dialog dialog, int style) {
         super.setupDialog(dialog, style);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            dialog.getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        }
         mView = View.inflate(getContext(), R.layout.fragment_bottom_odds_details, null);
         initView();
         dialog.setContentView(mView);
