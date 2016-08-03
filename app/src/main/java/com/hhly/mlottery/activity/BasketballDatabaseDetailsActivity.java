641a84266eebbb96c590c032fadb16d30ce2864c
loshine <longs@13322.com>
2016-08-03 17:45:51 +0800

    新增篮球资料库赛程页

--- app/src/main/java/com/hhly/mlottery/activity/BasketballDatabaseDetailsActivity.java
+++ app/src/main/java/com/hhly/mlottery/activity/BasketballDatabaseDetailsActivity.java
@@ -30,6 +30,7 @@ import com.hhly.mlottery.adapter.football.TabsAdapter;
 import com.hhly.mlottery.bean.basket.basketdatabase.BasketDatabaseBean;
 import com.hhly.mlottery.config.BaseURLs;
 import com.hhly.mlottery.frame.basketballframe.BasketDatabaseBigSmallFragment;
+import com.hhly.mlottery.frame.basketballframe.BasketDatabaseScheduleFragment;
 import com.hhly.mlottery.frame.basketballframe.BasketDatasaseHandicapFragment;
 import com.hhly.mlottery.util.L;
 import com.hhly.mlottery.util.MDStatusBarCompat;
@@ -44,6 +45,7 @@ import com.nostra13.universalimageloader.core.display.FadeInBitmapDisplayer;
 import com.umeng.analytics.MobclickAgent;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -55,8 +57,7 @@ import java.util.Map;
  */
 public class BasketballDatabaseDetailsActivity extends AppCompatActivity implements AppBarLayout.OnOffsetChangedListener, View.OnClickListener, SwipeRefreshLayout.OnRefreshListener {
 
-
-    public static final String LEAGUEID="leagueId";
+    public static final String LEAGUEID = "leagueId";
 
     private ViewPager mViewPager;
     private CollapsingToolbarLayout mCollapsingToolbarLayout;
@@ -83,6 +84,7 @@ public class BasketballDatabaseDetailsActivity extends AppCompatActivity impleme
 
     private ExactSwipeRefrashLayout mRefreshLayout; //下拉刷新
 
+    private BasketDatabaseScheduleFragment mBasketDatabaseScheduleFragment;
     private BasketDatasaseHandicapFragment mBasketDatasaseHandicapFragment;
     private BasketDatabaseBigSmallFragment mBasketDatabaseBigSmallFragment;
 
@@ -104,26 +106,26 @@ public class BasketballDatabaseDetailsActivity extends AppCompatActivity impleme
         /**不统计当前Activity*/
         MobclickAgent.openActivityDurationTrack(false);
 
-        if(getIntent().getExtras() != null){
+        if (getIntent().getExtras() != null) {
             mLeagueId = getIntent().getExtras().getString(LEAGUEID);
         }
 
         /**
          * 第一次加载默认赛季数据，不需要season ==》（-1）
          */
+        mBasketDatabaseScheduleFragment = BasketDatabaseScheduleFragment.newInstance();
         mBasketDatasaseHandicapFragment = BasketDatasaseHandicapFragment.newInstance(mLeagueId, "-1");
-        mBasketDatabaseBigSmallFragment = BasketDatabaseBigSmallFragment.newInstance(mLeagueId , "-1");
+        mBasketDatabaseBigSmallFragment = BasketDatabaseBigSmallFragment.newInstance(mLeagueId, "-1");
 
         mOptions = new DisplayImageOptions.Builder()
-                .cacheInMemory(true).cacheOnDisc(true)
+                .cacheInMemory(true).cacheOnDisk(true)
                 .imageScaleType(ImageScaleType.EXACTLY_STRETCHED)
                 .bitmapConfig(Bitmap.Config.RGB_565)// 防止内存溢出的，多图片使用565
                 .showImageForEmptyUri(R.mipmap.basket_default)
                 .showImageOnFail(R.mipmap.basket_default)// 加载失败显示的图片
                 .build();
 
-
-        mOptionsHead = new DisplayImageOptions.Builder().cacheInMemory(true).cacheOnDisc(true)
+        mOptionsHead = new DisplayImageOptions.Builder().cacheInMemory(true).cacheOnDisk(true)
                 .imageScaleType(ImageScaleType.EXACTLY_STRETCHED)
                 .bitmapConfig(Bitmap.Config.RGB_565)// 防止内存溢出的，多图片使用565
                 .showImageOnLoading(R.color.colorPrimary)
@@ -149,20 +151,25 @@ public class BasketballDatabaseDetailsActivity extends AppCompatActivity impleme
             initData();
         }
     };
+
     /**
      * 初始化界面
      */
     private void initView() {
-        TITLES = new String[]{getResources().getString(R.string.basket_database_details_handicapname),getResources().getString(R.string.basket_database_details_bigsmallname)};
+        TITLES = new String[]{
+                getString(R.string.basket_database_details_schedule),
+                getString(R.string.basket_database_details_handicapname),
+                getString(R.string.basket_database_details_bigsmallname)
+        };
 
         toolbar = (Toolbar) findViewById(R.id.basket_database_details_toolbar);
         setSupportActionBar(toolbar);
 
-        mIcon = (ImageView)findViewById(R.id.basket_details_guest_icon);
-        mBackground = (ImageView)findViewById(R.id.image_background);
-        mLeaguename = (TextView)findViewById(R.id.basket_database_leaguename);
+        mIcon = (ImageView) findViewById(R.id.basket_details_guest_icon);
+        mBackground = (ImageView) findViewById(R.id.image_background);
+        mLeaguename = (TextView) findViewById(R.id.basket_database_leaguename);
 
-        mSportsText = (TextView)findViewById(R.id.basket_database_datails_tv);
+        mSportsText = (TextView) findViewById(R.id.basket_database_datails_tv);
 
         mBasketLayoutHeader = (LinearLayout) findViewById(R.id.basket_database_details_img);
         mCollapsingToolbarLayout = (CollapsingToolbarLayout) findViewById(R.id.toolbar_layout);
@@ -175,14 +182,14 @@ public class BasketballDatabaseDetailsActivity extends AppCompatActivity impleme
 
         MDStatusBarCompat.setCollapsingToolbar(this, mCoordinatorLayout, appBarLayout, mBasketLayoutHeader, toolbar);
 
-
-        mTabsAdapter.addFragments(mBasketDatasaseHandicapFragment, mBasketDatabaseBigSmallFragment);
-        mViewPager.setOffscreenPageLimit(2);//设置预加载页面的个数。
+        mTabsAdapter.addFragments(mBasketDatabaseScheduleFragment,
+                mBasketDatasaseHandicapFragment,
+                mBasketDatabaseBigSmallFragment);
+        mViewPager.setOffscreenPageLimit(3);//设置预加载页面的个数。
         mViewPager.setAdapter(mTabsAdapter);
         mTabLayout.setupWithViewPager(mViewPager);
 
         appBarLayout.addOnOffsetChangedListener(this);
-
 
         headLayout = (LinearLayout) findViewById(R.id.basket_database_details_header_layout);
 
@@ -224,11 +231,13 @@ public class BasketballDatabaseDetailsActivity extends AppCompatActivity impleme
         mCollect.setOnClickListener(this);
 
     }
+
     private String[] mSports; // 赛季集
 
     private String mCurrentSports = "";
     private boolean isLoad = false;//赛季是否可筛选
-    private void initData(){
+
+    private void initData() {
 
         // http://192.168.31.43:8888/mlottery/core/basketballData.findLeagueHeader.do?lang=zh&leagueId=2
 //        String url = "http://192.168.31.43:8888/mlottery/core/basketballData.findLeagueHeader.do"; //
@@ -254,7 +263,7 @@ public class BasketballDatabaseDetailsActivity extends AppCompatActivity impleme
                         //若赛季无数据时 设为"--" 防止异常崩溃
                         if (mSports == null || mSports.length == 0) {
                             mSportsText.setText("--");
-                        }else{
+                        } else {
                             mSportsText.setText(mSports[0] + getResources().getString(R.string.basket_database_details_season));
                         }
                     } else {
@@ -336,7 +345,7 @@ public class BasketballDatabaseDetailsActivity extends AppCompatActivity impleme
     private boolean isFragment1 = false;
     private boolean is1 = false;
 
-    private void isHindShow(int position){
+    private void isHindShow(int position) {
         switch (position) {
             case 0:// 让分盘
                 isFragment0 = true;
@@ -406,37 +415,35 @@ public class BasketballDatabaseDetailsActivity extends AppCompatActivity impleme
      */
     @Override
     public void onRefresh() {
-            new Handler().postDelayed(new Runnable() {
-                @Override
-                public void run() {
-                    mRefreshLayout.setRefreshing(false);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                mRefreshLayout.setRefreshing(false);
 
-                    mHandlerData.postDelayed(mRun, 500); // 加载数据
-                    mBasketDatasaseHandicapFragment.upDate();
-                    mBasketDatabaseBigSmallFragment.upDate();
-                }
-            }, 1000);
+                mHandlerData.postDelayed(mRun, 500); // 加载数据
+                mBasketDatasaseHandicapFragment.upDate();
+                mBasketDatabaseBigSmallFragment.upDate();
+            }
+        }, 1000);
     }
 
 
     int currentDialogPosition = 0; // 当前选中的赛季（默认第一个）
     int currentPosition = 0; // 赛季选择过程中记录（点击确定后才赋值 ， 解决点击筛选不确定后再次进入赛季选择显示不一的情况）
 
-    public void setDialog(){
+    public void setDialog() {
         // Dialog 设置
         AlertDialog.Builder mBuilder = new AlertDialog.Builder(this, R.style.AlertDialog);
-        LayoutInflater infla = LayoutInflater.from(this);
-        View view = infla.inflate(R.layout.sports_alertdialog, null);
+        LayoutInflater inflater = LayoutInflater.from(this);
+        View view = inflater.inflate(R.layout.sports_alertdialog, null);
         TextView titleView = (TextView) view.findViewById(R.id.titleView);
-        Button dataOk = (Button)view.findViewById(R.id.sports_btn_ok);
+        Button dataOk = (Button) view.findViewById(R.id.sports_btn_ok);
         titleView.setText(getResources().getString(R.string.basket_database_details_season));
 
         final List<String> data = new ArrayList<>();
-        for (int i = 0; i < mSports.length; i++) {
-            data.add(mSports[i]);
-        }
+        Collections.addAll(data, mSports);
 
-        final SportsDialogAdapter mAdapter = new SportsDialogAdapter(data ,this , currentDialogPosition);
+        final SportsDialogAdapter mAdapter = new SportsDialogAdapter(data, this, currentDialogPosition);
 
         final AlertDialog mAlertDialog = mBuilder.create();
         mAlertDialog.setCanceledOnTouchOutside(true);//设置空白处点击 dialog消失
@@ -444,8 +451,8 @@ public class BasketballDatabaseDetailsActivity extends AppCompatActivity impleme
         /**
          * 根据List数据条数加载不同的ListView （数据多加载可滑动 ScrollTouchListview）
          */
-        ScrollView scroll = (ScrollView)view.findViewById(R.id.basket_sports_scroll);//数据多时显示
-        ScrollTouchListView  scrollListview = (ScrollTouchListView ) view.findViewById(R.id.sport_date_scroll);
+        ScrollView scroll = (ScrollView) view.findViewById(R.id.basket_sports_scroll);//数据多时显示
+        ScrollTouchListView scrollListview = (ScrollTouchListView) view.findViewById(R.id.sport_date_scroll);
         ListView listview = (ListView) view.findViewById(R.id.sport_date);//数据少时显示
         if (data.size() > 5) {
             scrollListview.setAdapter(mAdapter);
---
+++ app/src/main/java/com/hhly/mlottery/frame/basketballframe/BasketDatabaseScheduleFragment.java
@@ -0,0 +1,52 @@
+package com.hhly.mlottery.frame.basketballframe;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+import com.hhly.mlottery.R;
+
+/**
+ * 描    述：篮球资料库赛程
+ * 作    者：longs@13322.com
+ * 时    间：2016/8/3
+ */
+
+public class BasketDatabaseScheduleFragment extends Fragment {
+
+    TextView mTitleTextView;
+    ImageButton mLeftButton;
+    ImageButton mRightButton;
+    RecyclerView mRecyclerView;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_basket_database_schedule, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        mTitleTextView = (TextView) view.findViewById(R.id.title_button);
+        mLeftButton = (ImageButton) view.findViewById(R.id.left_button);
+        mRightButton = (ImageButton) view.findViewById(R.id.right_button);
+        mRecyclerView = (RecyclerView) view.findViewById(R.id.recycler_view);
+    }
+
+    public static BasketDatabaseScheduleFragment newInstance() {
+
+        Bundle args = new Bundle();
+
+        BasketDatabaseScheduleFragment fragment = new BasketDatabaseScheduleFragment();
+        fragment.setArguments(args);
+        return fragment;
+    }
+}
