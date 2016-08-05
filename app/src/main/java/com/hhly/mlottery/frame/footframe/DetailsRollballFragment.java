de2c7fd82335a81fc5a80f05dfbf298abfb611e0
wangg <wangg@13322.com>
2016-08-05 09:21:29 +0800

    修复足球内页闪退

--- app/src/main/java/com/hhly/mlottery/frame/footframe/DetailsRollballFragment.java
+++ app/src/main/java/com/hhly/mlottery/frame/footframe/DetailsRollballFragment.java
@@ -233,6 +233,7 @@ public class DetailsRollballFragment extends Fragment implements HappySocketClie
         allMatchLiveMsgId = new ArrayList<>();
         for (MatchTextLiveBean ml : matchLive) {
 
+
             if (ml.getMsgId() != null && !"".equals(ml.getMsgId())) {
                 allMatchLiveMsgId.add(Integer.parseInt(ml.getMsgId()));
             }
