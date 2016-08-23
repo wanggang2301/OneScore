42f5cf1ae674e2336851f07d2d70b5b4e17f432a
tangrr <trangrr@13322.com>
2016-08-23 12:17:02 +0800

    修复其它手机开启聊天室 时样式的适配问题

--- app/src/main/java/com/hhly/mlottery/frame/ConversationFragment.java
+++ app/src/main/java/com/hhly/mlottery/frame/ConversationFragment.java
@@ -1,13 +1,17 @@
 package com.hhly.mlottery.frame;
 
 import android.content.Context;
+import android.graphics.Rect;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.ViewTreeObserver;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.LinearLayout;
 import android.widget.Toast;
 
 import com.hhly.mlottery.R;
@@ -34,19 +38,43 @@ public class ConversationFragment extends FragmentActivity implements View.OnCli
 
     private UserInfo mUserInfo;// 当前用户信息
     private Context mContext;
+    private LinearLayout ll_comment;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            //透明状态栏
+            getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+            //透明导航栏
+            getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
+        }
+
         setContentView(R.layout.conversationlist);
         mContext = this;
+        ll_comment = (LinearLayout) findViewById(R.id.ll_comment);
         // 设置聊天室背景色为透明
         findViewById(R.id.conversation).setBackgroundColor(getResources().getColor(R.color.chart_room_bg_color));
 
         findViewById(R.id.ll_top).setOnClickListener(this);
 
         this.overridePendingTransition(R.anim.slide_in_from_bottom,0);// 开启动画
+
+        // 状态栏设为透明后软键盘监听失效，这里重新加载软键盘监听
+        final View decorView = getWindow().getDecorView();
+        decorView.getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+            @Override
+            public void onGlobalLayout() {
+                Rect r = new Rect();
+                decorView.getWindowVisibleDisplayFrame(r);
+                int screenHeight = decorView.getRootView().getHeight();
+                int heightDifference = screenHeight - r.bottom;
+                LinearLayout.LayoutParams params = ( LinearLayout.LayoutParams) ll_comment.getLayoutParams();
+                params.setMargins(0,0,0,heightDifference);
+                ll_comment.requestLayout();
+            }
+        });
 
 
         String userTestPhoto = "http://m.1332255.com/news/upload/shortcut/69c426f0f0974d4b8aae0826da71f751.png";// 用户使用测试头像
