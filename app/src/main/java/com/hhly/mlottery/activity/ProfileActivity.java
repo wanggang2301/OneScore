5d5ae53eb2a8d329ebebf0377ddcab4661cbadec
Yluyao <Yluyao>
2016-08-26 09:56:46 +0800

    修复头像内页样式调整

--- app/src/main/java/com/hhly/mlottery/activity/ProfileActivity.java
+++ app/src/main/java/com/hhly/mlottery/activity/ProfileActivity.java
@@ -319,6 +319,8 @@ public class ProfileActivity extends BaseActivity implements View.OnClickListene
                     Bitmap bitmap = MediaStore.Images.Media.getBitmap(ProfileActivity.this.getContentResolver(), uri);
                     if (bitmap.getWidth() < width || bitmap.getHeight() < height) {
                         UiUtils.toast(MyApp.getInstance(),R.string.see_your_face);
+                        bitmap.recycle();
+                        System.gc();
                         //successNull();
                         return;
                     }
@@ -334,6 +336,8 @@ public class ProfileActivity extends BaseActivity implements View.OnClickListene
                     if (bitmap.getWidth() < width || bitmap.getHeight() < height) {
                        // successNull();
                         UiUtils.toast(MyApp.getInstance(), R.string.see_your_face);
+                        bitmap.recycle();
+                        System.gc();
                         return;
                     }
                 } catch (Exception e) {
@@ -397,6 +401,8 @@ public class ProfileActivity extends BaseActivity implements View.OnClickListene
                     }
                     mPopupWindow. dismiss();
                     mHead_portrait.setImageBitmap(photo); //把图片显示在ImageView控件上
+                    photo.recycle();
+                    System.gc();
                 }
                 break;
             default:
