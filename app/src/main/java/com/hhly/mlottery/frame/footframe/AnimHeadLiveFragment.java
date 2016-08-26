fd8735b4bd75b9358d7a5ecb8aed4fa1e698ac19
wangg <wangg@13322.com>
2016-08-26 09:40:38 +0800

    足球内页动画直播国际化

--- app/src/main/java/com/hhly/mlottery/frame/footframe/AnimHeadLiveFragment.java
+++ app/src/main/java/com/hhly/mlottery/frame/footframe/AnimHeadLiveFragment.java
@@ -14,6 +14,7 @@ import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.TextView;
 
+import com.hhly.mlottery.MyApp;
 import com.hhly.mlottery.R;
 import com.hhly.mlottery.config.BaseURLs;
 import com.hhly.mlottery.widget.ProgressWebView;
@@ -67,7 +68,7 @@ public class AnimHeadLiveFragment extends Fragment {
         mWebView = (ProgressWebView) mView.findViewById(R.id.webview);
 
 
-        url = BaseURLs.URL_FOOTBALLDETAIL_H5 + "?thirdId=" + thirdId;
+        url = BaseURLs.URL_FOOTBALLDETAIL_H5 + "?thirdId=" + thirdId + "&lang=" + appendLanguage();
 
         // loadAnim();
 
@@ -90,7 +91,6 @@ public class AnimHeadLiveFragment extends Fragment {
         webSettings.setUseWideViewPort(true);
         webSettings.setBuiltInZoomControls(false);
 
-
         mWebView.setWebViewClient(new WebViewClient() {
             @Override
             public boolean shouldOverrideUrlLoading(WebView view, String url) {
@@ -109,5 +109,38 @@ public class AnimHeadLiveFragment extends Fragment {
         mWebView.loadUrl(url);
     }
 
+
+    /**
+     * 根据选择语言，改变推送接口语言环境
+     *
+     * @return
+     */
+    private String appendLanguage() {
+        String lang = "zh";//默认中文
+        if (MyApp.isLanguage.equals("rCN")) {
+            // 如果是中文简体的语言环境
+            lang = BaseURLs.LANGUAGE_SWITCHING_CN;
+        } else if (MyApp.isLanguage.equals("rTW")) {
+            // 如果是中文繁体的语言环境
+            lang = BaseURLs.LANGUAGE_SWITCHING_TW;
+        } else if (MyApp.isLanguage.equals("rEN")) {
+            // 如果是英文环境
+            lang = BaseURLs.LANGUAGE_SWITCHING_EN;
+        } else if (MyApp.isLanguage.equals("rKO")) {
+            // 如果是韩语环境
+            lang = BaseURLs.LANGUAGE_SWITCHING_KO;
+        } else if (MyApp.isLanguage.equals("rID")) {
+            // 如果是印尼语
+            lang = BaseURLs.LANGUAGE_SWITCHING_ID;
+        } else if (MyApp.isLanguage.equals("rTH")) {
+            // 如果是泰语
+            lang = BaseURLs.LANGUAGE_SWITCHING_TH;
+        } else if (MyApp.isLanguage.equals("rVI")) {
+            // 如果是越南语
+            lang = BaseURLs.LANGUAGE_SWITCHING_VI;
+        }
+
+        return lang.trim();
+    }
 
 }
