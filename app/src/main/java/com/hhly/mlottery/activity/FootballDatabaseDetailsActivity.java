8350981206ff8071f195e85b748c349c0229714b
Administrator <123456>
2016-09-02 10:48:18 +0800

    新增足球资料库详情

---
+++ app/src/main/java/com/hhly/mlottery/activity/FootballDatabaseDetailsActivity.java
@@ -0,0 +1,12 @@
+package com.hhly.mlottery.activity;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Administrator on 2016/9/2.
+ */
+
+public class FootballDatabaseDetailsActivity extends AppCompatActivity{
+
+
+}
