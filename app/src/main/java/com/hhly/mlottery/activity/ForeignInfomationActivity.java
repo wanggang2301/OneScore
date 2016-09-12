4949aa921085e77dcaa45ef450cf6e87fd8112e4
wangg <wangg@13322.com>
2016-09-12 16:21:10 +0800

    足球赛事提点日期完善

--- app/src/main/java/com/hhly/mlottery/activity/ForeignInfomationActivity.java
+++ app/src/main/java/com/hhly/mlottery/activity/ForeignInfomationActivity.java
@@ -1,15 +1,83 @@
 package com.hhly.mlottery.activity;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.view.ViewGroup;
 
+import com.chad.library.adapter.base.BaseQuickAdapter;
 import com.hhly.mlottery.R;
+import com.hhly.mlottery.adapter.ForeignInfomationAdapter;
 
-public class ForeignInfomationActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class ForeignInfomationActivity extends BaseActivity {
+
+    @BindView(R.id.recycler_view)
+    RecyclerView recyclerView;
+
+    private ForeignInfomationAdapter foreignInfomationAdapter;
+
+    private List<Integer> list;
+    private View moreView;  //加载更多
+
+    private View mView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.frag_foreign_infomation);
+        ButterKnife.bind(this);
+
+
+        recyclerView.setLayoutManager(new LinearLayoutManager(getApplicationContext(), LinearLayoutManager.VERTICAL, false));
+
+        setList();
+
+        foreignInfomationAdapter = new ForeignInfomationAdapter(getApplicationContext(), list);
+
+        recyclerView.setAdapter(foreignInfomationAdapter);
+
+        // moreView = View.inflate(this, R.layout.view_load_more, null);
+        moreView = getLayoutInflater().inflate(R.layout.view_load_more, (ViewGroup) recyclerView.getParent(), false);
+
+
+        foreignInfomationAdapter.openLoadMore(0, true);
+
+        foreignInfomationAdapter.setLoadingView(moreView);
+
+
+        foreignInfomationAdapter.setOnLoadMoreListener(new BaseQuickAdapter.RequestLoadMoreListener() {
+            @Override
+            public void onLoadMoreRequested() {
+                recyclerView.post(new Runnable() {
+                    @Override
+                    public void run() {
+
+
+                        for (int i = 0; i < 10; i++) {
+                            list.add(new Random().nextInt(50));
+                        }
+
+                         foreignInfomationAdapter.notifyDataChangedAfterLoadMore(true);
+                        // loadMoreData();
+                    }
+                });
+            }
+        });
+    }
+
+    private void setList() {
+
+        list = new ArrayList<>();
+        for (int i = 1; i < 20; i++) {
+            list.add(i);
+        }
     }
 }
---
+++ app/src/main/java/com/hhly/mlottery/adapter/ForeignInfomationAdapter.java
@@ -0,0 +1,42 @@
+package com.hhly.mlottery.adapter;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+
+import com.chad.library.adapter.base.BaseQuickAdapter;
+import com.chad.library.adapter.base.BaseViewHolder;
+import com.hhly.mlottery.R;
+
+import java.util.List;
+
+/**
+ * 描述:  ${TODO}
+ * 作者:  wangg@13322.com
+ * 时间:  2016/9/12 12:11
+ */
+public class ForeignInfomationAdapter extends BaseQuickAdapter<Integer> {
+    private Context mContext;
+    private List<Integer> list;
+
+    public ForeignInfomationAdapter(Context context, List<Integer> data) {
+        super(R.layout.item_foreign_infomation, data);
+        this.mContext = context;
+        this.list = data;
+    }
+
+
+    @Override
+    public int getViewHolderPosition(RecyclerView.ViewHolder viewHolder) {
+        return super.getViewHolderPosition(viewHolder);
+    }
+
+    @Override
+    protected void convert(BaseViewHolder viewHolder, Integer b) {
+
+
+        viewHolder.setText(R.id.tv_time, b + "分钟以前");
+
+    }
+
+}
+
