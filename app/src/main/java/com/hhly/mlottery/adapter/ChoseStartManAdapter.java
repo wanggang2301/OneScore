6dcfaf80ff564e124fad6485f811104aca9cb4a1
Yluyao <Yluyao>
2016-11-30 15:25:42 +0800

    New changelist

--- app/src/main/java/com/hhly/mlottery/adapter/ChoseStartManAdapter.java
+++ app/src/main/java/com/hhly/mlottery/adapter/ChoseStartManAdapter.java
@@ -5,12 +5,20 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
 import com.hhly.mlottery.R;
+import com.hhly.mlottery.activity.AvatarSelectionActivity;
 import com.hhly.mlottery.bean.ChoseStartBean;
+import com.hhly.mlottery.util.GlideUtils;
 import com.hhly.mlottery.util.ImageLoader;
+import com.hhly.mlottery.util.ListDatasSaveUtils;
 import com.hhly.mlottery.util.UiUtils;
 import com.hhly.mlottery.util.adapter.CommonAdapter;
 import com.hhly.mlottery.util.adapter.ViewHolder;
+import com.orhanobut.hawk.Hawk;
+import com.orhanobut.hawk.HawkBuilder;
+import com.orhanobut.hawk.LogLevel;
 
 import java.util.List;
 
@@ -32,7 +40,9 @@ public class ChoseStartManAdapter extends CommonAdapter<ChoseStartBean.DataBean.
         super(context, datas, layoutId);
         this.mContext = context;
         this.datas = datas;
+
     }
+
 
 
     public void setSeclection(int position) {
@@ -49,8 +59,11 @@ public class ChoseStartManAdapter extends CommonAdapter<ChoseStartBean.DataBean.
        this.holder=holder;
         chose_head_child = holder.getView(R.id.chose_head_child);
         imageView4 = holder.getView(R.id.imageView4);
-        ImageLoader.load(mContext, femaleBean.getHeadIcon(), R.mipmap.center_head).into(chose_head_child);
-
+        Glide.with(mContext).load( femaleBean.getHeadIcon())
+                .diskCacheStrategy(DiskCacheStrategy.ALL).into(chose_head_child);
+       /* ImageLoader.load(mContext, femaleBean.getHeadIcon(), R.mipmap.center_head)
+        .into(chose_head_child);*/
+      //  GlideUtils.loadImage(mContext,femaleBean.getHeadIcon(),chose_head_child);
         if (clickStatus==position) {
             holder.setBackgroundRes(R.id.imageView4, R.mipmap.right2x);
         }else {
