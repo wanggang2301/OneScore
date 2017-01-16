c71245a6fc4b89ad68b2b8b4c1a47d87f7ba6c6a
wangg <wangg@13322.com>
2017-01-16 15:04:06 +0800

    邀请码分享

--- app/src/main/java/com/hhly/mlottery/activity/InvitedActivity.java
+++ app/src/main/java/com/hhly/mlottery/activity/InvitedActivity.java
@@ -1,6 +1,5 @@
 package com.hhly.mlottery.activity;
 
-import android.app.Activity;
 import android.content.ClipboardManager;
 import android.content.Context;
 import android.os.Bundle;
@@ -19,12 +18,13 @@ import com.hhly.mlottery.util.net.VolleyContentFast;
 
 import java.util.HashMap;
 import java.util.Map;
+
 /**
  * Created by yuely198 on 2017/1/13.
  * 获取邀请码页面
  */
 
-public class InvitedActivity extends Activity implements View.OnClickListener{
+public class InvitedActivity extends BaseActivity implements View.OnClickListener {
 
     private TextView iv_invited_number;
     private TextView iv_number_copy;
@@ -43,18 +43,18 @@ public class InvitedActivity extends Activity implements View.OnClickListener{
         Map<String, String> param = new HashMap<>();
 
         param.put("userId", AppConstants.register.getData().getUser().getUserId());
-        Log.i("sada","ada"+ AppConstants.register.getData().getUser().getUserId());
-        String url="http://m.1332255.com:81/mlottery/core/androidUserCenter.getInviteCode.do";
+        Log.i("sada", "ada" + AppConstants.register.getData().getUser().getUserId());
+        String url = "http://m.1332255.com:81/mlottery/core/androidUserCenter.getInviteCode.do";
 
         VolleyContentFast.requestJsonByGet(url, param, new VolleyContentFast.ResponseSuccessListener<InvitedBean>() {
             @Override
             public void onResponse(InvitedBean bean) {
 
-                if(bean.getResult()==0){
+                if (bean.getResult() == 0) {
                     copy_text = bean.getData().getInviteCode();
                     iv_invited_number.setText(bean.getData().getInviteCode());
 
-                }else{
+                } else {
                     iv_invited_number.setText(R.string.invitation_code_acquisition_failed);
                 }
 
@@ -63,7 +63,7 @@ public class InvitedActivity extends Activity implements View.OnClickListener{
         }, new VolleyContentFast.ResponseErrorListener() {
             @Override
             public void onErrorResponse(VolleyContentFast.VolleyException exception) {
-               // UiUtils.toast(InvitedActivity.this, R.string.picture_put_failed);
+                // UiUtils.toast(InvitedActivity.this, R.string.picture_put_failed);
                 iv_invited_number.setText(R.string.invitation_code_acquisition_failed);
 
             }
@@ -94,31 +94,29 @@ public class InvitedActivity extends Activity implements View.OnClickListener{
 
     @Override
     public void onClick(View v) {
-        switch (v.getId()){
+        switch (v.getId()) {
             case R.id.public_img_back:
                 finish();
-            break;
+                break;
             case R.id.public_btn_save:  //分享
 
 
-                ShareBean shareBean=new ShareBean();
+                ShareBean shareBean = new ShareBean();
 
-                ShareFragment shareFragment=ShareFragment.newInstance(shareBean);
-               // shareFragment.show(getSupportFragmentManager(),"InvitedActivity");
+                ShareFragment shareFragment = ShareFragment.newInstance(shareBean);
+                shareFragment.show(getSupportFragmentManager(), "InvitedActivity");
 
 
-            break;
+                break;
             case R.id.iv_number_copy:
-                if (!copy_text.isEmpty()){
+                if (!copy_text.isEmpty()) {
                     ClipboardManager cmb = (ClipboardManager) getApplicationContext().getSystemService(Context.CLIPBOARD_SERVICE);
                     cmb.setText(copy_text);
-                    UiUtils.toast(getApplicationContext(),R.string.copy_text);
+                    UiUtils.toast(getApplicationContext(), R.string.copy_text);
                 }
                 break;
             default:
                 break;
-
-
 
 
         }
