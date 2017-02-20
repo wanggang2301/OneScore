11b8ccdd29fb69ae6c38d9194c7d68ac23c72ec0
wangg <wangg@13322.com>
2017-02-20 09:53:15 +0800

    斯洛克资料库01

--- app/src/main/java/com/hhly/mlottery/frame/datafrag/SnookerInfomationFragment.java
+++ app/src/main/java/com/hhly/mlottery/frame/datafrag/SnookerInfomationFragment.java
@@ -35,7 +35,6 @@ public class SnookerInfomationFragment extends Fragment {
         // Required empty public constructor
     }
 
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         mView = inflater.inflate(R.layout.fragment_snooker_infomation, container, false);
