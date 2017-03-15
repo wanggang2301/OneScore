61296bc4aefacd8fc661c28e9a6b9ee2ee2bbd58
Administrator <123456>
2017-03-15 17:47:13 +0800

    资料库日期国际化后显示问题

--- app/src/main/java/com/hhly/mlottery/util/DateUtil.java
+++ app/src/main/java/com/hhly/mlottery/util/DateUtil.java
@@ -173,6 +173,53 @@ public class DateUtil {
     }
 
     /**
+     * 判断字符串是否为日期格式 yyyy-MM-dd
+     * @param s
+     * @return
+     */
+    public static boolean isValidDateYMD(String s)
+    {
+        // 指定日期格式为四位年/两位月份/两位日期，注意yyyy/MM/dd区分大小写；
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+        // 设置lenient为false. 否则SimpleDateFormat会比较宽松地验证日期，比如2007/02/29会被接受，并转换成2007/03/01
+        dateFormat.setLenient(false);
+        try
+        {
+            dateFormat.parse(s);
+            return true;
+        }
+        catch (Exception e)
+        {
+            // 如果throw java.text.ParseException或者NullPointerException，就说明格式不对
+            return false;
+        }
+
+    }
+    /**
+     * 判断字符串是否为日期格式 MM-dd
+     * @param s
+     * @return
+     */
+    public static boolean isValidDateYM(String s)
+    {
+        // 指定日期格式为四位年/两位月份/两位日期，注意yyyy/MM/dd区分大小写；
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM");
+        // 设置lenient为false. 否则SimpleDateFormat会比较宽松地验证日期，比如2007/02/29会被接受，并转换成2007/03/01
+        dateFormat.setLenient(false);
+        try
+        {
+            dateFormat.parse(s);
+            return true;
+        }
+        catch (Exception e)
+        {
+            // 如果throw java.text.ParseException或者NullPointerException，就说明格式不对
+            return false;
+        }
+
+    }
+
+    /**
      * 把毫秒转化成日期
      *
      * @param millSec(毫秒数)
