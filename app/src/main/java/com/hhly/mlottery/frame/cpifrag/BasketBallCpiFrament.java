dda84b55d133ed73ba559ab99db543a7049c0e90
wangg <wangg@13322.com>
2017-03-16 11:49:15 +0800

    v128  足球指数优化

---
+++ app/src/main/java/com/hhly/mlottery/frame/cpifrag/BasketBallCpiFrament.java
@@ -0,0 +1,27 @@
+package com.hhly.mlottery.frame.cpifrag;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.hhly.mlottery.R;
+
+/**
+ * @author wangg
+ * @des:篮球指数
+ * @date:2017/3/16
+ */
+public class BasketBallCpiFrament extends Fragment {
+
+    public BasketBallCpiFrament() {
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_basket_ball_cpi, container, false);
+    }
+}
