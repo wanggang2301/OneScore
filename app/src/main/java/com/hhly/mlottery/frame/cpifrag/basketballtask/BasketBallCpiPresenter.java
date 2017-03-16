3e8a96cecb7af63dfcdddd316476cefe346dee37
wangg <wangg@13322.com>
2017-03-16 12:16:30 +0800

    v128  足球指数优化

---
+++ app/src/main/java/com/hhly/mlottery/frame/cpifrag/basketballtask/BasketBallCpiPresenter.java
@@ -0,0 +1,11 @@
+package com.hhly.mlottery.frame.cpifrag.basketballtask;
+
+/**
+ * @author: Wangg
+ * @Name：BasketBallCpiPresenter
+ * @Description:
+ * @Created on:2017/3/16  12:11.
+ */
+
+public class BasketBallCpiPresenter {
+}
