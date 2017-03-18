9cd5cd1673b16024152fcf332ea44793179a149e
Administrator <tangrr@m13322.com>
2017-03-18 11:54:44 +0800

    修改为国内环境

--- app/src/main/java/com/hhly/mlottery/util/AppConstants.java
+++ app/src/main/java/com/hhly/mlottery/util/AppConstants.java
@@ -16,7 +16,7 @@ public class AppConstants {
     /**
      * true是国际版|false内地版
      */
-    public static final boolean isGOKeyboard = true;
+    public static final boolean isGOKeyboard = false;
     /**
      * true是纯净版|false完整版
      */
