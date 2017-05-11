0bde37f38b6ecf2cb59a11cc9640bd107193f556
wangg <wangg@13322.com>
2017-05-11 12:24:04 +0800

    足球内页分析修改

--- app/src/main/java/com/hhly/mlottery/frame/footballframe/RecommendFragment.java
+++ app/src/main/java/com/hhly/mlottery/frame/footballframe/RecommendFragment.java
@@ -15,7 +15,6 @@ import com.hhly.mlottery.R;
  */
 public class RecommendFragment extends Fragment {
 
-
     public RecommendFragment() {
         // Required empty public constructor
     }
@@ -25,12 +24,8 @@ public class RecommendFragment extends Fragment {
         return recommendFragment;
     }
 
-
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_recommend, container, false);
     }
-
 }
