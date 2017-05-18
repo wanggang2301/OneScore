544f9a18066f1444d5af0e7bf460c9431c430293
Administrator <123456>
2017-05-18 10:44:56 +0800

    定制逻辑调整

---
+++ app/src/main/java/com/hhly/mlottery/activity/FocusListActivity.java
@@ -0,0 +1,19 @@
+package com.hhly.mlottery.activity;
+
+import android.os.Bundle;
+
+import com.hhly.mlottery.R;
+
+/**
+ * Created by：XQyi on 2017/5/17 10:55
+ * Use: 新版关注列表（足篮球）
+ */
+public class FocusListActivity extends BaseActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.focus_activity);
+    }
+}
