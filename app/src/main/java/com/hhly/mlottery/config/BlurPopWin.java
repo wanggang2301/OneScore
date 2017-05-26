0b689064ee3c8960c5383a9e007fe421aaf93ba7
Administrator <123456>
2017-05-26 09:05:10 +0800

    内页推荐列表

--- app/src/main/java/com/hhly/mlottery/config/BlurPopWin.java
+++ app/src/main/java/com/hhly/mlottery/config/BlurPopWin.java
@@ -5,11 +5,11 @@ import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.Rect;
 import android.graphics.drawable.BitmapDrawable;
+import android.support.annotation.IdRes;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.annotation.StringRes;
 import android.support.annotation.UiThread;
-import android.support.v7.widget.CardView;
 import android.text.InputType;
 import android.view.Gravity;
 import android.view.MotionEvent;
@@ -19,9 +19,10 @@ import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.PopupWindow;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.hhly.mlottery.R;
 import com.hhly.mlottery.util.L;
@@ -32,20 +33,34 @@ import com.hhly.mlottery.util.L;
  */
 public class BlurPopWin {
     private LinearLayout pop_root_layout;
-//    private RelativeLayout pop_layout;
-//    private TextView title;
     private TextView content;
     private ImageView close;
     private Builder builder;
     private PopupWindow popupWindow;
     private int radius;
     private float touchY;
+    private float touchX;
     private Bitmap localBit;
 
     public static final String GRAVITY_BOTTOM = "BOTTOM";
     public static final String GRAVITY_CENTER = "CENTER";
     private EditText editText;
     private TextView fabuText;
+    private LinearLayout record;
+    private RelativeLayout pop_layout;
+    private RadioGroup playGroup;
+    private RadioButton playRadioSpf;
+    private RadioButton playRadioLqspf;
+    private RadioGroup spfGroup;
+    private RadioButton spfRadioS;
+    private RadioButton spfRadioP;
+    private RadioButton spfRadioF;
+    private int playType;//推荐玩法选择
+    private int spfType;//胜平负选择
+    private LinearLayout radioPlay;
+    private LinearLayout radioRestart;
+    private LinearLayout radioYuYin;
+    private LinearLayout playYuYin;
 
     public BlurPopWin(Builder builder) {
         this.builder = builder;
@@ -106,28 +121,129 @@ public class BlurPopWin {
             View rootView = builder.activity.getLayoutInflater().inflate(R.layout.betting_issue_popup, null, false);
             popupWindow = new PopupWindow(rootView, ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT, true);
             pop_root_layout = (LinearLayout) rootView.findViewById(R.id.pop_root_layout);
-//            pop_layout = (RelativeLayout) rootView.findViewById(R.id.popup_ll);
-//            title = (TextView) rootView.findViewById(R.id.title);
-//            content = (TextView) rootView.findViewById(R.id.content);
+            pop_layout = (RelativeLayout) rootView.findViewById(R.id.popup_ll);
             editText = (EditText) rootView.findViewById(R.id.edit_query);
             editText.setInputType(InputType.TYPE_CLASS_NUMBER | InputType.TYPE_NUMBER_FLAG_DECIMAL);
             close = (ImageView) rootView.findViewById(R.id.close_popup_img);
             fabuText = (TextView) rootView.findViewById(R.id.fabu_text);
-            fabuText.setOnClickListener(new View.OnClickListener() {
+            record = (LinearLayout)rootView.findViewById(R.id.issue_record);
+            //推荐玩法
+            playGroup = (RadioGroup)rootView.findViewById(R.id.play_gendergroup);
+            playRadioSpf = (RadioButton)rootView.findViewById(R.id.play_spf_radio);
+            playRadioSpf.setChecked(true);
+            playRadioLqspf = (RadioButton)rootView.findViewById(R.id.play_lqspf_radio);
+            //胜平负
+            spfGroup = (RadioGroup)rootView.findViewById(R.id.spf_gendergroup);
+            spfRadioS = (RadioButton)rootView.findViewById(R.id.spf_radio_s);
+            spfRadioS.setChecked(true);
+            spfRadioP = (RadioButton)rootView.findViewById(R.id.spf_radio_p);
+            spfRadioF = (RadioButton)rootView.findViewById(R.id.spf_radio_f);
+
+            radioYuYin = (LinearLayout)rootView.findViewById(R.id.issue_radio_yuyin);
+            playYuYin = (LinearLayout)rootView.findViewById(R.id.issue_play_yuyin);
+
+            //语音播放
+            radioPlay = (LinearLayout)rootView.findViewById(R.id.radio_play);
+            //重新录制
+            radioRestart = (LinearLayout)rootView.findViewById(R.id.radio_restart);
+
+            playGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
                 @Override
-                public void onClick(View v) {
-                    String editData = editText.getText().toString();
-                    if (editData == null || editData.length() == 0) {
-                        L.d("qwer_asd*editData = " , "*");
-                    }else{
-                        L.d("qwer_asd*editData = " , editData);
+                public void onCheckedChanged(RadioGroup group, @IdRes int checkedId) {
+
+                    if (checkedId == playRadioSpf.getId()) {
+                        playType = ConstantPool.PLAY_SPF;
+                        L.d("qwer_asd" , " SPF " + playType);
+                    }else if (checkedId == playRadioLqspf.getId()) {
+                        playType = ConstantPool.PLAY_LQSPF;
+                        L.d("qwer_asd" , " LQSPF " + playType);
+                    }
+                    if (builder.playRadioGroupOnclick != null) {
+                        builder.playRadioGroupOnclick.onPlayRadioGroupOnclick(checkedId , playType);
                     }
                 }
             });
 
-//            if (builder.title != null) {
-//                title.setText(builder.title);
-//            }
+            spfGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+                @Override
+                public void onCheckedChanged(RadioGroup group, @IdRes int checkedId) {
+                    if (checkedId == spfRadioS.getId()) {
+                        spfType = ConstantPool.SPF_S;
+                        L.d("qwer_asd" , " S " + checkedId);
+                    }else if (checkedId == spfRadioP.getId()) {
+                        spfType = ConstantPool.SPF_P;
+                        L.d("qwer_asd" , " P " + checkedId);
+                    }else if (checkedId == spfRadioF.getId()) {
+                        spfType = ConstantPool.SPF_F;
+                        L.d("qwer_asd" , " F " + checkedId);
+                    }
+                    if (builder.spfRadioGroupOnclick != null) {
+                        builder.spfRadioGroupOnclick.onSpfRadioGroupOnclick(checkedId , spfType);
+                    }
+                }
+            });
+
+            record.setLongClickable(true);
+            record.setOnLongClickListener(new View.OnLongClickListener() {
+                @Override
+                public boolean onLongClick(View v) {
+                    if (builder.longRecordOnclick != null) {
+                        builder.longRecordOnclick.onLongRecordOnClick(BlurPopWin.this);
+                    }
+                    return true;
+                }
+            });
+            record.setOnTouchListener(new View.OnTouchListener() {
+                @Override
+                public boolean onTouch(View v, MotionEvent event) {
+                    if (builder.touchRecordOnclick != null) {
+                        builder.touchRecordOnclick.onTouchRecordClick(event.getAction());
+                    }
+                    switch (event.getAction()){
+                        case MotionEvent.ACTION_DOWN:
+                            break;
+                        case MotionEvent.ACTION_UP:
+                            radioYuYin.setVisibility(View.GONE);
+                            playYuYin.setVisibility(View.VISIBLE);
+                            break;
+                        case MotionEvent.ACTION_MOVE:
+                            break;
+                    }
+                    return false;
+                }
+            });
+            radioRestart.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    radioYuYin.setVisibility(View.VISIBLE);
+                    playYuYin.setVisibility(View.GONE);
+                }
+            });
+            radioPlay.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if (builder.radioPlayYuYin != null) {
+                        builder.radioPlayYuYin.onRadioPlayYuYin();
+                    }
+                }
+            });
+//            record.setOnClickListener(new View.OnClickListener() {
+//                @Override
+//                public void onClick(View v) {
+//                    if (builder.touchRecordOnclick != null) {
+//                        builder.touchRecordOnclick.onTouchRecordClick(BlurPopWin.this);
+//                    }
+//                }
+//            });
+            fabuText.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    String editData = editText.getText().toString();
+                    if (builder.popupCallback != null) {
+                        builder.popupCallback.onClick(BlurPopWin.this , editData);
+                    }
+                }
+            });
 
             if (builder.content != null) {
                 content.setText(builder.content);
@@ -138,10 +254,6 @@ public class BlurPopWin {
             } else {
                 radius = 5;
             }
-
-//            if (builder.titleTextSize != 0) {
-//                title.setTextSize(builder.titleTextSize);
-//            }
 
             if (builder.contentTextSize != 0) {
                 content.setTextSize(builder.contentTextSize);
@@ -160,7 +272,7 @@ public class BlurPopWin {
                 close.setVisibility(View.INVISIBLE);
             }
 
-//            RelativeLayout.LayoutParams lp = new RelativeLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+//            RelativeLayout.LayoutParams lp = new RelativeLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
 //            if (builder.showAtLocationType.equals(GRAVITY_CENTER)) {
 //                lp.addRule(RelativeLayout.CENTER_VERTICAL, RelativeLayout.TRUE);
 //            } else {
@@ -180,23 +292,35 @@ public class BlurPopWin {
                     switch (motionEvent.getAction()) {
                         case MotionEvent.ACTION_DOWN:
                             touchY = motionEvent.getY();
+                            touchX = motionEvent.getX();
                             break;
                         case MotionEvent.ACTION_MOVE:
                             break;
                         case MotionEvent.ACTION_UP:
 
                             if(builder.isBackgroundClose){
-                                if (builder.showAtLocationType.equals(GRAVITY_CENTER)) {
+                                if ((touchX < pop_layout.getLeft() || touchX > pop_layout.getRight())
+                                        &&(touchY < pop_layout.getTop() || touchY > pop_layout.getBottom())) {
+
+                                    popupWindow.dismiss();
+                                }
+//                                if (touchY < pop_layout.getTop() || touchY > pop_layout.getBottom() ) {
+//                                    popupWindow.dismiss();
+//                                }
+                                L.d("qwer_ad = ", "点击外层");
+//                                popupWindow.dismiss();
+//                                if (builder.showAtLocationType.equals(GRAVITY_CENTER)) {
 //                                    if (touchY < pop_layout.getTop() || touchY > pop_layout.getBottom()) {
 //                                        popupWindow.dismiss();
 //                                    }
-                                    L.d("qwer_ad = ", "点击外层");
-                                    popupWindow.dismiss();
-                                } else if (builder.showAtLocationType.equals(GRAVITY_BOTTOM)) {
+//                                    L.d("qwer_ad = ", "点击外层");
+//                                    popupWindow.dismiss();
+//                                }
+//                                else if (builder.showAtLocationType.equals(GRAVITY_BOTTOM)) {
 //                                    if (touchY < pop_layout.getTop()) {
 //                                        popupWindow.dismiss();
 //                                    }
-                                }
+//                                }
                             }
                             break;
                         default:
@@ -208,24 +332,22 @@ public class BlurPopWin {
         } else {
             throw new NullPointerException("---> BlurPopWin ---> initBlurPopWin --->builder=null");
         }
-//        pop_layout.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                if (builder.popupCallback != null) {
-//                    builder.popupCallback.onClick(BlurPopWin.this);
-//                }
-//            }
-//        });
         return this;
     }
 
     public static class Builder {
 
         protected BlurPopWin blurPopWin;
-        protected int titleTextSize, contentTextSize;
+//        protected int titleTextSize;
+        protected int contentTextSize;
         protected Activity activity;
         protected Context context;
         protected PopupCallback popupCallback;
+        protected TouchRecordOnclick touchRecordOnclick;
+        protected LongRecordOnClick longRecordOnclick;
+        protected PlayRadioGroupOnclick playRadioGroupOnclick;
+        protected SpfRadioGroupOnclick spfRadioGroupOnclick;
+        protected RadioPlayYuYin radioPlayYuYin;
         protected int radius;
         protected String title, content;
         protected boolean isCancelable;
@@ -245,6 +367,29 @@ public class BlurPopWin {
             return this;
         }
 
+        public Builder onTouchRecordClick(TouchRecordOnclick touchRecordOnclick){
+            this.touchRecordOnclick = touchRecordOnclick;
+            return this;
+        }
+        public Builder onLongRecordOnclick(LongRecordOnClick longRecordOnclick){
+            this.longRecordOnclick = longRecordOnclick;
+            return this;
+        }
+
+        public Builder onRadioGroupOnclick(PlayRadioGroupOnclick playRadioGroupOnclick){
+            this.playRadioGroupOnclick = playRadioGroupOnclick;
+            return this;
+        }
+
+        public Builder onSpfRadioGroupOnclick(SpfRadioGroupOnclick spfRadioGroupOnclick){
+            this.spfRadioGroupOnclick = spfRadioGroupOnclick;
+            return this;
+        }
+
+        public Builder onRadioPlayYuYin(RadioPlayYuYin radioPlayYuYin){
+            this.radioPlayYuYin = radioPlayYuYin;
+            return this;
+        }
 //        /*
 //        * 设置标题
 //        * */
@@ -327,9 +472,45 @@ public class BlurPopWin {
         }
 
     }
-
+    /**
+     * 发布按键接口
+     */
     public interface PopupCallback {
+        void onClick(@NonNull BlurPopWin blurPopWin ,String money);
+    }
 
-        void onClick(@NonNull BlurPopWin blurPopWin);
+    /**
+     * 录音按键接口
+     */
+    public interface TouchRecordOnclick {
+        void onTouchRecordClick(int event);
+    }
+
+    /**
+     * 长按录音监听
+     */
+    public interface LongRecordOnClick{
+        void onLongRecordOnClick(@NonNull BlurPopWin blurPopWin );
+    }
+
+    /**
+     * 推荐玩法监听
+     */
+    public interface PlayRadioGroupOnclick {
+         void onPlayRadioGroupOnclick(int checkedId , int playType);
+    }
+
+    /**
+     * 胜平负玩法监听
+     */
+    public interface SpfRadioGroupOnclick{
+        void onSpfRadioGroupOnclick(int checkedId , int spfType);
+    }
+
+    /**
+     * 语音播放监听
+     */
+    public interface RadioPlayYuYin{
+        void onRadioPlayYuYin();
     }
 }
