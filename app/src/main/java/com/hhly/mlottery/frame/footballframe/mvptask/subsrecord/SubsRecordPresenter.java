25d7bbac7089e1edf9ffa32ce1f22e44c113b872
wangg <wangg@13322.com>
2017-06-01 19:29:39 +0800

    足球内页滚球改版

---
+++ app/src/main/java/com/hhly/mlottery/frame/footballframe/mvptask/subsrecord/SubsRecordPresenter.java
@@ -0,0 +1,11 @@
+package com.hhly.mlottery.frame.footballframe.mvptask.subsrecord;
+
+/**
+ * @author: Wangg
+ * @name：xxx
+ * @description: xxx
+ * @created on:2017/6/1  19:13.
+ */
+
+public class SubsRecordPresenter {
+}
