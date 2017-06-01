286674d22ac9c259fd392bac3fca355394d35e03
wangg <wangg@13322.com>
2017-06-01 19:14:23 +0800

    足球内页滚球改版

---
+++ app/src/main/java/com/hhly/mlottery/frame/footballframe/mvptask/subsrecord/SubsRecordFragment.java
@@ -0,0 +1,37 @@
+package com.hhly.mlottery.frame.footballframe.mvptask.subsrecord;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.hhly.mlottery.R;
+
+/**
+ * @author wangg
+ * @desc 订阅记录SubsRecordFragment
+ * @date 2017 六一儿童节
+ */
+
+public class SubsRecordFragment extends Fragment {
+
+
+    public static SubsRecordFragment newInstance() {
+        SubsRecordFragment subsRecordFragment = new SubsRecordFragment();
+        return subsRecordFragment;
+    }
+
+
+    public SubsRecordFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_subs_record, container, false);
+    }
+
+}
