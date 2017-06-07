799f2d841ee44325644723a4844bf2e2cdb6660e
Andy <andy319go@163.com>
2017-06-07 15:12:46 +0800

    账户详情提交。优化布局。加载数据

--- app/src/main/java/com/hhly/mlottery/frame/accountdetail/AccountDetailPresenter.java
+++ app/src/main/java/com/hhly/mlottery/frame/accountdetail/AccountDetailPresenter.java
@@ -47,7 +47,7 @@ public class AccountDetailPresenter extends BasePresenter<AccountDetailContract.
 //        String userId=AppConstants.register.getUser().getUserId();
         String userId="HHLY00000136";
 //        String token=AppConstants.register.getToken();
-        String token="eyJhbGciOiJIUzI1NiJ9.eyJqdGkiOiJqd3QiLCJpYXQiOjE0OTY3NDkyNDUsInN1YiI6IntcImlkXCI6XCJISExZMDAwMDAxMzZcIixcInBob25lTnVtXCI6XCIxNTAxMzY5NzEwMVwiLFwiaW1hZ2VTcmNcIjpcImh0dHA6Ly90cC4xMzMyMjU1LmNvbS9pbWcvMjAxNy0wNS0xNi9hZjJkMTAzMi1iOWUyLTQ5YjYtODc5MS1iZDgyODMzNjQxOWEuanBnXCIsXCJuaWNrTmFtZVwiOlwi5biV5bCU5qKF5ouJ5pav5L2c5a6iMTox5oiY5bmz5Zyf5bqT5pu856ue5oqAXCJ9In0.KEG8wx5V2GaCRt7KNBDg_cooyiaTSOTvaUphUXFJCdw";
+        String token="eyJhbGciOiJIUzI1NiJ9.eyJqdGkiOiJqd3QiLCJpYXQiOjE0OTY4MDM3OTcsInN1YiI6IntcImlkXCI6XCJISExZMDAwMDAxMzZcIixcInBob25lTnVtXCI6XCIxNTAxMzY5NzEwMVwiLFwiaW1hZ2VTcmNcIjpcImh0dHA6Ly90cC4xMzMyMjU1LmNvbS9pbWcvMjAxNy0wNS0xNi9hZjJkMTAzMi1iOWUyLTQ5YjYtODc5MS1iZDgyODMzNjQxOWEuanBnXCIsXCJuaWNrTmFtZVwiOlwi5biV5bCU5qKF5ouJ5pav5L2c5a6iMTox5oiY5bmz5Zyf5bqT5pu856ue5oqAXCJ9In0.NWOL8XGDWDehovknsPSs_zUCBddsPdBRBH-7y7QIqdI";
 
         Observable<AccountDetailBean> observable=mRepository.getAccountData(userId,token,"");
         addSubscription(observable, new Subscriber<AccountDetailBean>() {
@@ -58,13 +58,14 @@ public class AccountDetailPresenter extends BasePresenter<AccountDetailContract.
 
             @Override
             public void onError(Throwable e) {
+                L.e("sign",e.getMessage()+"errror");
                 mView.onError();
             }
 
             @Override
             public void onNext(AccountDetailBean accountDetailBean) {
 //                ToastTools.showQuick(MyApp.get(),"sign"+accountDetailBean.getData().getBalance().getAvailableBalance()+"");
-                if(accountDetailBean.getCode().equals(200)&&accountDetailBean.getData()!=null&&accountDetailBean.getData().getRecord()!=null
+                if(accountDetailBean.getCode().equals("200")&&accountDetailBean.getData()!=null&&accountDetailBean.getData().getRecord()!=null
                         &&accountDetailBean.getData().getRecord().size()!=0){ //有数据
                     mListData.clear();
                     mListData.addAll(accountDetailBean.getData().getRecord());
@@ -85,8 +86,13 @@ public class AccountDetailPresenter extends BasePresenter<AccountDetailContract.
 
     @Override
     public void refreshDataByPage() {
-        String userId=AppConstants.register.getUser().getUserId();
-        String token=AppConstants.register.getToken();
+//        String userId=AppConstants.register.getUser().getUserId();
+//        String token=AppConstants.register.getToken();
+
+        String userId="HHLY00000136";
+//        String token=AppConstants.register.getToken();
+        String token="eyJhbGciOiJIUzI1NiJ9.eyJqdGkiOiJqd3QiLCJpYXQiOjE0OTY3MTY2NTEsInN1YiI6IntcImlkXCI6XCJISExZMDAwMDAxMzZcIixcInBob25lTnVtXCI6XCIxNTAxMzY5NzEwMVwifSJ9.DnxIwOrkdG--8mxn4-j8sGJ85ouF7hCiyPmCIdR8XYg";
+
         Observable<AccountPageBean> observable=mRepository.getAccountDataByPage(userId,token,"",mPage.index+"");
         addSubscription(observable, new Subscriber<AccountPageBean>() {
             @Override
@@ -101,7 +107,7 @@ public class AccountDetailPresenter extends BasePresenter<AccountDetailContract.
 
             @Override
             public void onNext(AccountPageBean accountDetailBean) {
-                if(accountDetailBean.getCode().equals(200)&&accountDetailBean.getData()!=null
+                if(accountDetailBean.getCode().equals("200")&&accountDetailBean.getData()!=null
                         &&accountDetailBean.getData().size()!=0){ //有数据
                     mView.showNextPage(accountDetailBean.getData());
                 }else {
