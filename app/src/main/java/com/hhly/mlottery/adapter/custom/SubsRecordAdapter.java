1e85e1034f421cecf9650e28e2b60e7796cd4014
wangg <wangg@13322.com>
2017-06-10 10:47:00 +0800

    订阅记录adapter单独写一个item,防止小强偷偷下毒

--- app/src/main/java/com/hhly/mlottery/adapter/custom/SubsRecordAdapter.java
+++ app/src/main/java/com/hhly/mlottery/adapter/custom/SubsRecordAdapter.java
@@ -35,7 +35,25 @@ public class SubsRecordAdapter extends BaseQuickAdapter<SubsRecordBean.PurchaseR
     }
 
     @Override
-    protected void convert(BaseViewHolder baseViewHolder, final SubsRecordBean.PurchaseRecordsBean.ListBean b) {
+    protected void convert(final BaseViewHolder baseViewHolder, final SubsRecordBean.PurchaseRecordsBean.ListBean b) {
+
+
+   /*     // Uri uri = new Uri.Builder().build();
+        Glide.with(mContext).load(b.getHeadImg()).asBitmap().into(new SimpleTarget<Bitmap>() {
+            @Override
+            public void onResourceReady(Bitmap resource, GlideAnimation<? super Bitmap> glideAnimation) {
+                baseViewHolder.setImageBitmap(R.id.betting_portrait_img, resource);
+            }
+
+            @Override
+            public void onLoadFailed(Exception e, Drawable errorDrawable) {
+                super.onLoadFailed(e, errorDrawable);
+                baseViewHolder.setImageResource(R.id.betting_portrait_img, R.mipmap.specialist_default);
+                //history_item_pic.setVisibility(View.GONE);
+            }
+        }); //方法中设置asBitmap可以设置回调类型
+*/
+
         Glide.with(mContext).load(b.getHeadImg()).placeholder(R.mipmap.specialist_default).into((CircleImageView) baseViewHolder.getView(R.id.betting_portrait_img));
         baseViewHolder.setText(R.id.betting_specialist_name, b.getNickName());
         baseViewHolder.setText(R.id.betting_specialist_grade, getSpecialistGrade(b.getLevels()));
@@ -44,7 +62,7 @@ public class SubsRecordAdapter extends BaseQuickAdapter<SubsRecordBean.PurchaseR
     /*    if (b.getWinpointThreeDays() == 0) {
             baseViewHolder.setVisible(R.id.betting_lately_accuracy, false);
         } else {*/
-       // baseViewHolder.setVisible(R.id.betting_lately_accuracy, true);
+        // baseViewHolder.setVisible(R.id.betting_lately_accuracy, true);
         baseViewHolder.setText(R.id.betting_lately_accuracy, getLastAccuracy(b.getWinpointThreeDays(), b.getErrpointThreeDays()));
         // }
 
