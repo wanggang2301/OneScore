0c744ee017c59c3ced088877c6ee95e4113799bc
Administrator <tangrr@m13322.com>
2017-06-10 15:47:29 +0800

    添加QQ粉丝 群功能

--- app/src/main/java/com/hhly/mlottery/activity/HomeUserOptionsActivity.java
+++ app/src/main/java/com/hhly/mlottery/activity/HomeUserOptionsActivity.java
@@ -415,34 +415,31 @@ public class HomeUserOptionsActivity extends Activity implements View.OnClickLis
                 }
                 break;
             case R.id.rl_my_join:// 加入QQ粉丝群
-                if (checkApkExist(this, "com.tencent.mobileqq")) {
-//                    startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("mqqwpa://im/chat?chat_type=wpa&uin=" + 332434723 + "&version=1")));// 进入QQ号
-                    startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("mqqwpa://im/chat?chat_type=group&uin=" + 332434723 + "&version=1")));// 进入QQ群
-                } else {
-                    Toast.makeText(this, getString(R.string.no_qq_installed), Toast.LENGTH_SHORT).show();
-                }
+                joinQQGroup("QVQ_ZzmrH5PocArnvvw9_U_hnv3Gs2Os");
                 break;
             default:
                 break;
         }
     }
 
-
-    /**
-     * 检测包名是否存在
+    /****************
      *
-     * @param context
-     * @param packageName
-     * @return
-     */
-    public boolean checkApkExist(Context context, String packageName) {
-        if (packageName == null || "".equals(packageName))
-            return false;
+     * 发起添加群流程。群号：一比分用户体验群(332434723) 的 key 为： QVQ_ZzmrH5PocArnvvw9_U_hnv3Gs2Os
+     * 调用 joinQQGroup(QVQ_ZzmrH5PocArnvvw9_U_hnv3Gs2Os) 即可发起手Q客户端申请加群 一比分用户体验群(332434723)
+     *
+     * @param key 由官网生成的key
+     * @return 返回true表示呼起手Q成功，返回fals表示呼起失败
+     ******************/
+    public boolean joinQQGroup(String key) {
+        Intent intent = new Intent();
+        intent.setData(Uri.parse("mqqopensdkapi://bizAgent/qm/qr?url=http%3A%2F%2Fqm.qq.com%2Fcgi-bin%2Fqm%2Fqr%3Ffrom%3Dapp%26p%3Dandroid%26k%3D" + key));
+        // 此Flag可根据具体产品需要自定义，如设置，则在加群界面按返回，返回手Q主界面，不设置，按返回会返回到呼起产品界面    //intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
         try {
-            ApplicationInfo info = context.getPackageManager().getApplicationInfo(packageName,
-                    PackageManager.GET_UNINSTALLED_PACKAGES);
+            startActivity(intent);
             return true;
-        } catch (PackageManager.NameNotFoundException e) {
+        } catch (Exception e) {
+            // 未安装手Q或安装的版本不支持
+            Toast.makeText(this, getString(R.string.no_qq_installed), Toast.LENGTH_SHORT).show();
             return false;
         }
     }
