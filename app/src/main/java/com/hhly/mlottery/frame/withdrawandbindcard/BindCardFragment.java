8573e6a184b0fe54eacbe9e4f81eea31bb0c95c0
Andy <andy319go@163.com>
2017-06-11 10:44:58 +0800

    提现功能

--- app/src/main/java/com/hhly/mlottery/frame/withdrawandbindcard/BindCardFragment.java
+++ app/src/main/java/com/hhly/mlottery/frame/withdrawandbindcard/BindCardFragment.java
@@ -58,7 +58,16 @@ public class BindCardFragment extends ViewFragment<BindCardContract.Presenter>im
         mSave.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                mPresenter.request(mName.getText().toString(),mBank.getText().toString(),mCardNumber.getText().toString());
+                if(mName.getText().toString().isEmpty()){
+                    Toast.makeText(mActivity, "开户名不能为空", Toast.LENGTH_SHORT).show();
+                }else if(mBank.getText().toString().isEmpty()){
+                    Toast.makeText(mActivity, "开户银行不能为空", Toast.LENGTH_SHORT).show();
+                }else if(checkBankCard(mCardNumber.getText().toString())){
+                    Toast.makeText(mActivity, "银行卡号不正确", Toast.LENGTH_SHORT).show();
+                }else{
+                    mPresenter.request(mName.getText().toString(),mBank.getText().toString(),mCardNumber.getText().toString());
+                }
+
             }
         });
         mBack.setOnClickListener(new View.OnClickListener() {
@@ -74,7 +83,43 @@ public class BindCardFragment extends ViewFragment<BindCardContract.Presenter>im
 
     }
 
-    @Override
+    /**
+     * 校验银行卡卡号
+     *
+     * @param cardId
+     * @return
+     */
+    public  boolean checkBankCard(String cardId) {
+        char bit = getBankCardCheckCode(cardId
+                .substring(0, cardId.length() - 1));
+        return cardId.charAt(cardId.length() - 1) == bit;
+    }
+    /**
+     * 从不含校验位的银行卡卡号采用 Luhm 校验算法获得校验位
+     *
+     * @param nonCheckCodeCardId
+     * @return
+     */
+    public  char getBankCardCheckCode(String nonCheckCodeCardId) {
+        int cardLenth = nonCheckCodeCardId.trim().length();
+        if (nonCheckCodeCardId == null || cardLenth == 0
+                || !nonCheckCodeCardId.matches("\\d+")) {
+            throw new IllegalArgumentException("不是银行卡的卡号!");
+        }
+        char[] chs = nonCheckCodeCardId.trim().toCharArray();
+        int luhmSum = 0;
+        for (int i = chs.length - 1, j = 0; i >= 0; i--, j++) {
+            int k = chs[i] - '0';
+            if (j % 2 == 0) {
+                k *= 2;
+                k = k / 10 + k % 10;
+            }
+            luhmSum += k;
+        }
+        return (luhmSum % 10 == 0) ? '0' : (char) ((10 - luhmSum % 10) + '0');
+    }
+
+        @Override
     public BindCardContract.Presenter initPresenter() {
         return new BindCardPresenter(this);
     }
